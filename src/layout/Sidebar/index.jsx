da526dca076d089aeca8432cb434676a0382dedc
Nguyễn Mạnh Tuấn <tuanmnguye@northstudio.vn>
2024-12-24 18:19:20 +0700

    :lipstick: shop sidebar

--- src/layout/Sidebar/index.jsx
+++ src/layout/Sidebar/index.jsx
@@ -3,64 +3,45 @@ import Drawer from './styles';
 
 // components
 import Logo from '@components/Logo';
-import {NavLink} from 'react-router-dom';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import Collapse from '@mui/material/Collapse';
-import {Fragment} from 'react';
+import { Fragment } from 'react';
 
 // hooks
-import {useSidebar} from '@contexts/sidebarContext';
-import {useState, useEffect} from 'react';
-import {useWindowSize} from 'react-use';
+import { useSidebar } from '@contexts/sidebarContext';
+import { useState, useEffect } from 'react';
+import { useWindowSize } from 'react-use';
 
 // constants
 import ROUTES from '@constants/routes';
 
 const Sidebar = () => {
-    const {width} = useWindowSize();
-    const {open, setOpen} = useSidebar();
-    const [active, setActive] = useState('Dashboard');
-    const isPermanent = width >= 1920;
+  const { width } = useWindowSize();
+  const { open, setOpen } = useSidebar();
+  const [active, setActive] = useState('Dashboard');
+  const isPermanent = width >= 1920;
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            setActive('');
-        });
 
-        return () => {
-            window.removeEventListener('resize', () => {
-                setActive('');
-            });
-        };
-    }, []);
-
-    return (
-      <Drawer
-        id="appMenu"
-        anchor="left"
-        transitionDuration={350}
-        open={open}
-        variant={isPermanent ? "permanent" : "temporary"}
-        onOpen={() => setOpen(true)}
-        onClose={() => setOpen(false)}
-      >
-        <Logo />
-        <nav className="menu">
+  return (
+    <div className='flex gap-8 h-full flex-1'>
+      <div className='col-span-1 h-[calc(100vh-150px)] card w-1/5'>
+        <div className="flex flex-col space-y-4">
           {ROUTES.map((route, index) => {
             return (
-              <Fragment key={route.name}>
+              <div key={route.name} className='cursor-pointer font-semibold'>
                 {route.links && (
                   <>
                     <div>
                       <div
-                        className={`menu_item ${
-                          active === route.name ? "active" : ""
-                        }`}
-                        onClick={() =>
+                        className={`flex items-center justify-between ${active === route.name ? "text-blue-700 font-bold" : ""
+                          }`}
+                        onClick={() => {
                           setActive(active === route.name ? "" : route.name)
-                        }
+                        }}
                       >
                         <div className="flex items-center gap-2.5">
-                            <i className={`icon icon-${route.icon}`} />
+                          <i className={`icon icon-${route.icon}`} />
                           <span className="text">{route.name}</span>
                         </div>
                         <button
@@ -75,13 +56,14 @@ const Sidebar = () => {
                         timeout="auto"
                         unmountOnExit
                       >
-                        <div className="submenu flex flex-col gap-2.5">
+                        <div className="flex flex-col gap-2.5 my-4 ms-3">
                           {route.links.map((link) => {
                             return (
                               <NavLink
-                                className="submenu_item menu_item"
-                                to={link.path}
+                                className={`${active === link.name ? "text-blue-700 font-bold" : ""}`}
+                                to={`/shop/${link.path}`}
                                 key={link.name}
+                                onClick={() => setActive(active === link.name ? "" : link.name)}
                               >
                                 <span className="flex items-center gap-2.5">
                                   <i className="icon icon-circle-solid" />
@@ -100,28 +82,32 @@ const Sidebar = () => {
                 )}
                 {!route.links && (
                   <>
-                    <NavLink className="menu_item" to={route.path}>
+                    <div
+                      className={`${active === route.name ? "text-blue-700 font-bold" : ""}`}
+                      onClick={() => {
+                        setActive(active === route.name ? "" : route.name)
+                        navigate(`/shop/${route.path}`)
+                      }}>
                       <div className="flex items-center gap-2.5">
-                          <i className={`icon icon-${route.icon}`} />
+                        <i className={`icon icon-${route.icon}`} />
                         <span className="text">{route.name}</span>
                       </div>
-                      {/* {route.qty && (
-                      <span className="badge rounded bg-green xl:hidden">
-                        {route.qty}
-                      </span>
-                    )} */}
-                    </NavLink>
+                    </div>
                     {index === ROUTES.length - 2 && (
                       <span className="menu_divider" />
                     )}
                   </>
                 )}
-              </Fragment>
+              </div>
             );
           })}
-        </nav>
-      </Drawer>
-    );
+        </div>
+      </div>
+      <div className='flex-1 overflow-y-auto mb-[40px] '>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar
