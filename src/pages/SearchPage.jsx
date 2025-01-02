b5c24f172d60486d8f017fde4949144afa593801
manh1111 <minhanh746347@gmail.com>
2025-01-02 12:17:57 +0700

    update search

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,33 +1,38 @@
 import React, { useEffect, useState, useCallback } from "react";
-import ProductCard from "@widgets/Shop/ProductCard";
-import { getCategoriesTree } from "@api/categorie";
-import { searchProduct } from "@api/product";
-import { useSearchProduct } from "@contexts/searchProductContext";
-import { CaretDownOutlined, CaretUpOutlined } from "@ant-design/icons";
+import ProductCard from "@widgets/Shop/ProductCard"; // Ensure this component is memoized
+import { getCategories } from "@api/categorie"; // API to get categories
+import { searchProduct } from "@api/product"; // Import the searchProduct function
+import { useSearchProduct } from "@contexts/searchProductContext"; // Custom hook for product search
 
 const SearchPage = () => {
   const {
     searchTerm,
+    setSearchTerm,
     filteredProducts,
     loadMoreProducts,
     updateProductList,
   } = useSearchProduct();
 
   const [categories, setCategories] = useState([]);
-  const [expandedCategories, setExpandedCategories] = useState({}); // Tracks expanded state
-  const [selectedSortOption, setSelectedSortOption] = useState("Phổ biến");
+  const [selectedSortOption, setSelectedSortOption] = useState("Mới nhất");
+  const [priceSortOption, setPriceSortOption] = useState("Giá");
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 8;
+  const itemsPerPage = 20;
   const [activeCategory, setActiveCategory] = useState("");
 
   const fetchCategories = async () => {
     try {
-      const catalogData = await getCategoriesTree();
+      const catalogData = await getCategories();
       setCategories(catalogData);
+      if (catalogData.length > 0) {
+        setActiveCategory(catalogData[0]._id);
+      }
     } catch (error) {
       console.error("Error fetching categories:", error);
     }
   };
+  
   useEffect(() => {
     fetchCategories();
   }, []);
@@ -39,7 +44,7 @@ const SearchPage = () => {
         category: categoryId,
         page,
         limit: itemsPerPage,
-        sortBy,
+        sortBy, 
       });
 
       updateProductList(data.productsWithCounts || []);
@@ -51,7 +56,6 @@ const SearchPage = () => {
   useEffect(() => {
     if (activeCategory) {
       const sortByMap = {
-        "Phổ biến": "sold_count",
         "Mới nhất": "-createdAt",
         "Giá thấp đến cao": "price_asc",
         "Giá cao đến thấp": "price_desc",
@@ -60,58 +64,33 @@ const SearchPage = () => {
       const sortBy = sortByMap[selectedSortOption];
       searchProducts(searchTerm, activeCategory, currentPage, sortBy);
     }
-  }, [searchTerm, activeCategory, currentPage, selectedSortOption, searchProducts]);
+  }, [
+    searchTerm,
+    activeCategory,
+    currentPage,
+    selectedSortOption,
+    searchProducts,
+  ]);
 
-  useEffect(() => {
-    // Extract the category ID from the URL query parameters
-    const params = new URLSearchParams(window.location.search);
-    const categoryFromUrl = params.get('category');
-    if (categoryFromUrl) {
-      setActiveCategory(categoryFromUrl);
-      setCurrentPage(1); // Reset to the first page
-    }
-  }, []); // Only run once on component mount
+  const handleSortOptionClick = useCallback((option) => {
+    setSelectedSortOption(option);
+    setDropdownOpen(false);
+    setCurrentPage(1);
+  }, []);
+
+  const handleSearch = useCallback(
+    (e) => {
+      const newSearchTerm = e.target.value;
+      setSearchTerm(newSearchTerm);
+      setCurrentPage(1);
+    },
+    [setSearchTerm]
+  );
 
   const handleCategoryClick = useCallback((categoryId) => {
     setActiveCategory(categoryId);
     setCurrentPage(1);
   }, []);
-
-  const toggleCategoryExpansion = (categoryId) => {
-    setExpandedCategories((prevState) => ({
-      ...prevState,
-      [categoryId]: !prevState[categoryId],
-    }));
-  };
-
-  const renderCategories = (categories, depth = 0) => {
-    return categories.map((category) => (
-      <div key={category._id} style={{ paddingLeft: depth * 4 }}>
-        <div
-          className={`cursor-pointer py-1 flex items-center justify-between ${
-            activeCategory === category._id ? "text-red font-bold" : "text-black"
-          }`}
-          onClick={() => handleCategoryClick(category._id)}
-        >
-          <span>{category.category_name}</span>
-          {category.children && category.children.length > 0 && (
-            <button
-              className=" text-sm text-gray-500"
-              onClick={(e) => {
-                e.stopPropagation(); 
-                toggleCategoryExpansion(category._id);
-              }}
-            >
-              {expandedCategories[category._id] ? <CaretUpOutlined /> : <CaretDownOutlined />}
-            </button>
-          )}
-        </div>
-        {expandedCategories[category._id] &&
-          category.children &&
-          renderCategories(category.children, depth + 1)}
-      </div>
-    ));
-  };
 
   const handlePageChange = useCallback(
     (newPage) => {
@@ -126,26 +105,74 @@ const SearchPage = () => {
   const totalPages = Math.ceil(filteredProducts.length / itemsPerPage);
 
   return (
-    <div className="flex flex-row w-full h-screen bg-white p-5 relative">
+    <div className="flex flex-row w-full h-full bg-white p-5 relative">
       {/* Sidebar for Categories */}
       <div className="w-1/4 bg-white p-5">
         <div className="flex items-center pb-2">
           <span className="font-bold text-xl">Danh Mục</span>
         </div>
-        <div>{renderCategories(categories)}</div>
+        <div>
+          {categories.map((category) => (
+            <div
+              key={category._id}
+              className={`flex items-center cursor-pointer py-1 ${
+                activeCategory === category._id
+                  ? "text-red font-bold"
+                  : "text-black"
+              }`}
+              onClick={() => handleCategoryClick(category._id)}
+            >
+              {category.category_name}
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Main Content */}
       <div className="w-full flex flex-col justify-between">
         <div>
-        {/* Product List */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {filteredProducts
-            .slice(0, currentPage * itemsPerPage)
-            .map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-        </div>
+          {/* Search Input
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearch}
+            placeholder="Tìm kiếm sản phẩm..."
+            className="border p-2 mb-4 w-full"
+          /> */}
+
+          <fieldset className="border-0 p-0 m-0">
+            <div className="font-bold mb-2 text-xl">Sắp xếp theo</div>
+            <div className="flex gap-2 mb-4">
+              <section className="flex gap-2">
+                {[
+                  "Mới nhất",
+                  "Giá thấp đến cao",
+                  "Giá cao đến thấp",
+                ].map((option) => (
+                  <button
+                    key={option}
+                    className={`px-4 py-2 border rounded transition-colors duration-300 ${
+                      selectedSortOption === option
+                        ? "bg-red text-white border-red"
+                        : "bg-transparent border-gray-300 text-gray-700"
+                    }`}
+                    onClick={() => handleSortOptionClick(option)}
+                  >
+                    <span aria-hidden="true">{option}</span>
+                  </button>
+                ))}
+              </section>
+            </div>
+          </fieldset>
+
+          {/* Product List */}
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {filteredProducts
+              .slice(0, currentPage * itemsPerPage)
+              .map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+          </div>
         </div>
 
         {/* Pagination Controls */}
